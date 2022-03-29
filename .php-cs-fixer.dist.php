7f41633ae4a9c0573be472ef6e2d7e1784d77649
Joren Van Hocht <joren.van.hocht@takethelead.be>
2022-03-29 13:48:06 +0200

    php cs config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -18,7 +18,7 @@ return $config
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'imports_order' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
         'trailing_comma_in_multiline' => ['elements' => ['arrays']],
